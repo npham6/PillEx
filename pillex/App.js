bb08f612dce81eeeeebd121d422414c96296cd5e
Nhi Pham <npham6@Nhis-MacBook-Pro.local>
2019-02-02 04:14:34 -0500

    Show me the money

--- pillex/App.js
+++ pillex/App.js
@@ -2,28 +2,35 @@ import React from 'react';
 import {Text, View,TextInput, Image} from 'react-native';
 import {Button} from 'react-native';
 import {ImageBackground} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
+import {LinearGradient} from 'expo';
 
 
 export default class App extends React.Component{
   render(){
     return(
       <View style={styles.wholeStyle}>
-        <View style={{backgroundColor: '#B2EBF2', flex:1}}>
-          {/* <LinearGradient 
-          colors={['(rgba(0,0,0,0.8)','transparent']}
-          style={styles.gradientStyle}>
+        
+          <LinearGradient 
+          colors={['rgba(238,174,202,1)','rgba(148,187,233,1)']}
+          start={[0,0]}
+          end={[1,1]}
+          location={[0,1]}
+          style={styles.gradientStyle}/>
           
-             <Text style={styles.header}> PillEx </Text>
+              <Text style={styles.header}> PillEx </Text>
             <TextInput
             style={styles.inputStyle}
-            // onChangeText
             placeholder="Please enter serial number"
             /> 
+            <View style={styles.buttonStyles}>
+          <Button 
+            title="Search"
+            color="#9FA8DA"
+          />
+         </View>
 
           
-          </LinearGradient> */}
-        </View>
+
       </View>
     )
   }
@@ -37,23 +44,25 @@ const styles = {
         justtifyContent: 'center'
     },
     gradientStyle: {
+        backgroundColor: 'rgb(148,187,233)',
         position:'absolute',
         left:0,
         right:0,
         top:0,
-        height:0
+        height:'100%'
     },
 
     inputStyle:{
-        height: 40,
-        width: "95%",
-        padding: 5,
-        borderColor: "#9FA8DA",
-        borderWidth: 1
+        flex:1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
         
     },
     header:{
         fontSize: 30,
+        alignItems: 'center',
+        justtifyContent: 'center',
         color: 'white',
         fontWeight: 'bold'
     },
